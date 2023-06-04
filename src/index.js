cb6cb517bcbfbdd749446796e0d34814103936d5
painter98 <emailmepallavi@gmail.com>
2023-06-04 19:12:44 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,19 +4,6 @@ import App from "./App";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 import "./style.css";
-/*import { BrowserRouter } from "react-router-dom";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
-);
-*/
 import { HashRouter } from "react-router-dom";
 
 //const history = createHashHistory();
